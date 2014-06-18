eee4530d7b8216338634282f3097cb96932aa28e
William McBrine <wmcbrine@gmail.com>
2014-06-18 18:38:35 -0400

    More readable display in browser; automatically quit after giving ten seconds to respond.

--- zwebbrowse.py
+++ zwebbrowse.py
@@ -1,30 +1,41 @@
 from zeroconf import *
 import socket
+import time
 
 class MyListener(object):
     def __init__(self):
         self.r = Zeroconf()
 
     def removeService(self, zeroconf, type, name):
+        print
         print "Service", name, "removed"
 
     def addService(self, zeroconf, type, name):
+        print
         print "Service", name, "added"
-        print "Type is", type
+        print "  Type is", type
         info = self.r.getServiceInfo(type, name)
         if info:
-            print "Address is", str(socket.inet_ntoa(info.getAddress()))
-            print "Port is", info.getPort()
-            print "Weight is", info.getWeight()
-            print "Priority is", info.getPriority()
-            print "Server is", info.getServer()
-            print "Text is", info.getText()
-            print "Properties are", info.getProperties()
+            print "  Address is %s:%d" % (socket.inet_ntoa(info.getAddress()),
+                                          info.getPort())
+            print "  Weight is %d, Priority is %d" % (info.getWeight(),
+                                                      info.getPriority())
+            print "  Server is", info.getServer()
+            text = info.getText()
+            if text:
+                print "  Text is", repr(text)
+            prop = info.getProperties()
+            if prop:
+                print "  Properties are"
+                for key, value in prop.items():
+                    print "    %s: %s" % (key, value)
 
 if __name__ == '__main__':
     print "Multicast DNS Service Discovery for Python Browser test"
     r = Zeroconf()
-    print "1. Testing browsing for a service..."
+    print "Testing browsing for a service..."
     type = "_http._tcp.local."
     listener = MyListener()
     browser = ServiceBrowser(r, type, listener)
+    time.sleep(10)
+    r.close()
