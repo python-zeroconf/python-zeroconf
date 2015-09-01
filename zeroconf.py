023426e0f8982640f46bca3dfcd3abeee2cb832f
Giovanni Harting <chefeificationful@gmail.com>
2015-09-01 23:47:40 +0200

    Support kernel versions <3.9
    added catch of OSError
    added catch of socket.error for python2

--- zeroconf.py
+++ zeroconf.py
@@ -1295,12 +1295,18 @@ def new_socket():
     # Volume 2"), but some BSD-derived systems require
     # SO_REUSEPORT to be specified explicity.  Also, not all
     # versions of Python have SO_REUSEPORT available.
+    # Catch OSError and socket.error for kernel versions <3.9 because lacking
+    # SO_REUSEPORT support.
     try:
         reuseport = socket.SO_REUSEPORT
     except AttributeError:
         pass
     else:
-        s.setsockopt(socket.SOL_SOCKET, reuseport, 1)
+        try:
+            s.setsockopt(socket.SOL_SOCKET, reuseport, 1)
+        except (OSError, socket.error) as err:  # OSError on python 3, socket.error on python 2
+            if not err.errno == errno.ENOPROTOOPT:
+                raise
 
     s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
