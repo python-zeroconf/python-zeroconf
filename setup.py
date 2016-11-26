73a316a83260d4e034883f59296c2ef0e8ef5e5f
hugovk <hugovk@users.noreply.github.com>
2016-11-26 11:06:22 +0200

    flake8 no longer supports Py2.6

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
