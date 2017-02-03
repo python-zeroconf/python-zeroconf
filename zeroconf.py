48b19498724825237d3002ee7681b6296c625b12
Jakub Stasiak <jakub@stasiak.at>
2017-02-03 14:14:20 +0000

    Release version 0.18.0

--- zeroconf.py
+++ zeroconf.py
@@ -41,7 +41,7 @@ from six.moves import xrange
 
 __author__ = 'Paul Scott-Murphy, William McBrine'
 __maintainer__ = 'Jakub Stasiak <jakub@stasiak.at>'
-__version__ = '0.17.7'
+__version__ = '0.18.0'
 __license__ = 'LGPL'
 
 
