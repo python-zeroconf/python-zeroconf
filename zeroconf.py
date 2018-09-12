python-zeroconf/python-zeroconf

commit aa1f48433cbd4dbf52565ec0c2635e5d52a37086

Improve test coverage, and fix issues found

zeroconf.py
@@ -409,8 +409,8 @@ class DNSRecord(DNSEntry):
         self.created = current_time_millis()
 
     def __eq__(self, other):
-        """Tests equality as per DNSRecord"""
-        return isinstance(other, DNSRecord) and DNSEntry.__eq__(self, other)
+        """Abstract method"""
+        raise AbstractMethodException
 
     def suppressed_by(self, msg):
         """Returns true if any answer in a message can suffice for the
@@ -478,10 +478,9 @@ class DNSAddress(DNSRecord):
     def __repr__(self):
         """String representation"""
         try:
-            return socket.inet_ntoa(self.address)
-        except Exception as e:  # TODO stop catching all Exceptions
-            log.exception('Unknown error, possibly benign: %r', e)
-            return self.address
+            return str(socket.inet_ntoa(self.address))
+        except Exception:  # TODO stop catching all Exceptions
+            return str(self.address)
 
 
 class DNSHinfo(DNSRecord):
@@ -773,7 +772,7 @@ class DNSOutgoing(object):
         adding_answers = 2
         adding_authoratives = 3
         adding_additionals = 4
-        finished = 4
+        finished = 5
 
     def set_state(self, state):
         if self.state != state:
@@ -1489,9 +1488,7 @@ class ServiceInfo(object):
 
     def __eq__(self, other):
         """Tests equality of service name"""
-        if isinstance(other, ServiceInfo):
-            return other.name == self.name
-        return False
+        return isinstance(other, ServiceInfo) and other.name == self.name
 
     def __ne__(self, other):
         """Non-equality test"""
@@ -1696,7 +1693,6 @@ class Zeroconf(QuietLogger):
         info = ServiceInfo(type_, name)
         if info.request(self, timeout):
             return info
-        return None
 
     def add_service_listener(self, type_, listener):
         """Adds a listener for a particular service type.  This object
@@ -1963,7 +1959,7 @@ class Zeroconf(QuietLogger):
         """Sends an outgoing packet."""
         packet = out.packet()
         if len(packet) > _MAX_MSG_ABSOLUTE:
-            self.log_warning_once("Dropping %r over-sided packet (%d bytes) %r",
+            self.log_warning_once("Dropping %r over-sized packet (%d bytes) %r",
                                   out, len(packet), packet)
             return
         log.debug('Sending %r (%d bytes) as %r...', out, len(packet), packet)
@@ -1972,9 +1968,9 @@ class Zeroconf(QuietLogger):
                 return
             bytes_sent = s.sendto(packet, 0, (addr, port))
             if bytes_sent != len(packet):
-                raise Error(
-                    'Should not happen, sent %d out of %d bytes' % (
-                        bytes_sent, len(packet)))
+                self.log_warning_once(
+                    '!!! sent %d out of %d bytes to %r' % (
+                        bytes_sent, len(packet)), s)
 
     def close(self):
         """Ends the background threads, and prevent this instance from

test_zeroconf.py
@@ -38,6 +38,60 @@ def teardown_module():
     log.setLevel(original_logging_level[0])
 
 
+class TestDunder(unittest.TestCase):
+
+    def test_dns_text_repr(self):
+        # There was an issue on Python 3 that prevented DNSText's repr
+        # from working when the text was longer than 10 bytes
+        text = DNSText('irrelevant', None, 0, 0, b'12345678901')
+        repr(text)
+
+        text = DNSText('irrelevant', None, 0, 0, b'123')
+        repr(text)
+
+    def test_dns_hinfo_repr_eq(self):
+        hinfo = DNSHinfo('irrelevant', r._TYPE_HINFO, 0, 0, 'cpu', 'os')
+        assert hinfo == hinfo
+        repr(hinfo)
+
+    def test_dns_pointer_repr(self):
+        pointer = r.DNSPointer(
+            'irrelevant', r._TYPE_PTR, r._CLASS_IN, r._DNS_TTL, '123')
+        repr(pointer)
+
+    def test_dns_address_repr(self):
+        address = r.DNSAddress('irrelevant', r._TYPE_SOA, r._CLASS_IN, 1, b'a')
+        repr(address)
+
+    def test_dns_question_repr(self):
+        question = r.DNSQuestion(
+            'irrelevant', r._TYPE_SRV, r._CLASS_IN | r._CLASS_UNIQUE)
+        repr(question)
+        assert not question != question
+
+    def test_dns_service_repr(self):
+        service = r.DNSService(
+            'irrelevant', r._TYPE_SRV, r._CLASS_IN, r._DNS_TTL, 0, 0, 80, b'a')
+        repr(service)
+
+    def test_dns_record_abc(self):
+        record = r.DNSRecord('irrelevant', r._TYPE_SRV, r._CLASS_IN, r._DNS_TTL)
+        self.assertRaises(r.AbstractMethodException, record.__eq__, record)
+        self.assertRaises(r.AbstractMethodException, record.write, None)
+
+    def test_service_info_dunder(self):
+        type_ = "_test-srvc-type._tcp.local."
+        name = "xxxyyy"
+        registration_name = "%s.%s" % (name, type_)
+        info = ServiceInfo(
+            type_, registration_name,
+            socket.inet_aton("10.0.1.2"), 80, 0, 0,
+            None, "ash-2.local.")
+
+        assert not info != info
+        repr(info)
+
+
 class PacketGeneration(unittest.TestCase):
 
     def test_parse_own_packet_simple(self):
@@ -364,7 +418,6 @@ class ServiceTypesQuery(unittest.TestCase):
         try:
             service_types = ZeroconfServiceTypes.find(
                 interfaces=['127.0.0.1'], timeout=0.5)
-            # print(service_types)
             assert discovery_type in service_types
             service_types = ZeroconfServiceTypes.find(
                 zc=zeroconf_registrar, timeout=0.5)
@@ -395,8 +448,9 @@ class ListenerTest(unittest.TestCase):
             def remove_service(self, zeroconf, type, name):
                 service_removed.set()
 
+        listener = MyListener()
         zeroconf_browser = Zeroconf(interfaces=['127.0.0.1'])
-        zeroconf_browser.add_service_listener(subtype, MyListener())
+        zeroconf_browser.add_service_listener(subtype, listener)
 
         properties = dict(
             prop_none=None,
@@ -445,6 +499,7 @@ class ListenerTest(unittest.TestCase):
             assert service_removed.is_set()
         finally:
             zeroconf_registrar.close()
+            zeroconf_browser.remove_service_listener(listener)
             zeroconf_browser.close()
 
 
@@ -482,10 +537,3 @@ def test_integration():
         zeroconf_registrar.close()
         browser.cancel()
         zeroconf_browser.close()
-
-
-def test_dnstext_repr_works():
-    # There was an issue on Python 3 that prevented DNSText's repr
-    # from working when the text was longer than 10 bytes
-    text = DNSText('irrelevant', None, 0, 0, b'12345678901')
-    repr(text)

commit 23fdcce35fa020d09267e6fa57cf21cfb744a2c4

Fix a logging call
The format string expects three parameters, one of them was accidentally
passed to the log_warning_once() method instead.

Fixes: aa1f48433cbd ("Improve test coverage, and fix issues found")

zeroconf.py
@@ -2079,7 +2079,7 @@ class Zeroconf(QuietLogger):
                 if bytes_sent != len(packet):
                     self.log_warning_once(
                         '!!! sent %d out of %d bytes to %r' % (
-                            bytes_sent, len(packet)), s)
+                            bytes_sent, len(packet), s))
 
     def close(self):
         """Ends the background threads, and prevent this instance from