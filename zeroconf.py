python-zeroconf/python-zeroconf

commit ff4a262adc6926905c71e2952b3159b84a974d02

Allow underscores in incoming service names
There are real world cases of services broadcasting names with
underscores in them so tough luck, let's accept those to be compatible.
Registering service names with underscores in them continues to be
disallowed.

Closes https://github.com/jstasiak/python-zeroconf/issues/102

zeroconf.py
@@ -152,6 +152,7 @@ _TYPES = {_TYPE_A: "a",
 
 _HAS_A_TO_Z = re.compile(r'[A-Za-z]')
 _HAS_ONLY_A_TO_Z_NUM_HYPHEN = re.compile(r'^[A-Za-z0-9\-]+$')
+_HAS_ONLY_A_TO_Z_NUM_HYPHEN_UNDERSCORE = re.compile(r'^[A-Za-z0-9\-\_]+$')
 _HAS_ASCII_CONTROL_CHARS = re.compile(r'[\x00-\x1f\x7f]')
 
 int2byte = struct.Struct(">B").pack
@@ -177,7 +178,7 @@ def current_time_millis() -> float:
     return time.time() * 1000
 
 
-def service_type_name(type_):
+def service_type_name(type_, *, allow_underscores: bool = False):
     """
     Validate a fully qualified service name, instance or subtype. [rfc6763]
 
@@ -255,10 +256,15 @@ def service_type_name(type_):
             "Service name (%s) must contain at least one letter (eg: 'A-Z')" %
             name)
 
-    if not _HAS_ONLY_A_TO_Z_NUM_HYPHEN.search(name):
+    allowed_characters_re = (
+        _HAS_ONLY_A_TO_Z_NUM_HYPHEN_UNDERSCORE if allow_underscores
+        else _HAS_ONLY_A_TO_Z_NUM_HYPHEN
+    )
+
+    if not allowed_characters_re.search(name):
         raise BadTypeInNameException(
             "Service name (%s) must contain only these characters: "
-            "A-Z, a-z, 0-9, hyphen ('-')" % name)
+            "A-Z, a-z, 0-9, hyphen ('-')%s" % (name, ", underscore ('_')" if allow_underscores else ""))
 
     if remaining and remaining[-1] == '_sub':
         remaining.pop()

test_zeroconf.py
@@ -521,6 +521,8 @@ class Exceptions(unittest.TestCase):
         for name in good_names_to_try:
             r.service_type_name(name)
 
+        r.service_type_name('_one_two._tcp.local.', allow_underscores=True)
+
 
 class TestDnsIncoming(unittest.TestCase):
 

commit ae3bd517d84aae631db1cc294caf22541a7f4bd5

Actually allow underscores in incoming service names
This was meant to be released earlier, but I failed to merge part of my
patch.

Fixes: ff4a262adc69 ("Allow underscores in incoming service names")
Closes #102

zeroconf.py
@@ -1270,7 +1270,7 @@ class ServiceBrowser(RecordUpdateListener, threading.Thread):
                  addr: str = _MDNS_ADDR, port: int = _MDNS_PORT, delay: int = _BROWSER_TIME) -> None:
         """Creates a browser for a specific type"""
         assert handlers or listener, 'You need to specify at least one handler'
-        if not type_.endswith(service_type_name(type_)):
+        if not type_.endswith(service_type_name(type_, allow_underscores=True)):
             raise BadTypeInNameException
         threading.Thread.__init__(
             self, name='zeroconf-ServiceBrowser_' + type_)
@@ -1403,7 +1403,7 @@ class ServiceInfo(RecordUpdateListener):
                     bytes for the text field)
         server: fully qualified name for service host (defaults to name)"""
 
-        if not type_.endswith(service_type_name(name)):
+        if not type_.endswith(service_type_name(name, allow_underscores=True)):
             raise BadTypeInNameException
         self.type = type_
         self.name = name