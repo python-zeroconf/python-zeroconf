b35c6bf38fa75fefbfad658f02f96daf9c2f115d
Andrew Bonney <andrew@sqwarkr.com>
2019-03-15 13:42:23 +0000

    tests: speed up integration test

--- test_zeroconf.py
+++ test_zeroconf.py
@@ -876,7 +876,7 @@ def test_integration():
             time_offset += expected_ttl / 4
             zeroconf_browser.notify_all()
             sleep_count += 1
-            got_query.wait(1)
+            got_query.wait(0.1)
             got_query.clear()
             # Prevent the test running indefinitely in an error condition
             assert sleep_count < test_iterations * 4
