0f7366423fab8369700be086f3007c20897fde1f
Erik <erik@montnemery.com>
2020-08-18 16:07:03 +0200

    Remove initial delay before querying for service info

--- zeroconf/__init__.py
+++ zeroconf/__init__.py
@@ -1894,7 +1894,7 @@ class ServiceInfo(RecordUpdateListener):
         """
         now = current_time_millis()
         delay = _LISTENER_TIME
-        next_ = now + delay
+        next_ = now
         last = now + timeout
 
         record_types_for_check_cache = [(_TYPE_SRV, _CLASS_IN), (_TYPE_TXT, _CLASS_IN)]
