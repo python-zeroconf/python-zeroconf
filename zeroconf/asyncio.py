67f10ac5b979121ea04f1ce62f5266a2f07dcceb
J. Nick Koston <nick@koston.org>
2021-05-26 16:24:35 -1000

    ci

--- zeroconf/asyncio.py
+++ zeroconf/asyncio.py
@@ -137,4 +137,6 @@ class AsyncZeroconf:
     async def async_close(self) -> None:
         """Ends the background threads, and prevent this instance from
         servicing further queries."""
+        import pprint
+        pprint.pprint(self.zeroconf.cache.cache)
         await self.loop.run_in_executor(None, self.zeroconf.close)
