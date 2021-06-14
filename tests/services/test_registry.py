fe72524dbaf934ca63ebce053e34f3e838743460
J. Nick Koston <nick@koston.org>
2021-06-13 14:07:09 -1000

    Fix lookup of uppercase names in registry (#597)
    - If the ServiceInfo was registered with an uppercase name and the query was
      for a lowercase name, it would not be found and vice-versa.

--- tests/services/test_registry.py
+++ tests/services/test_registry.py
@@ -46,3 +46,41 @@ class TestServiceRegistry(unittest.TestCase):
         assert registry.get_infos_type(type_) == [info]
         assert registry.get_infos_server("ash-2.local.") == [info]
         assert registry.get_types() == [type_]
+
+    def test_lookups_upper_case_by_lower_case(self):
+        type_ = "_test-SRVC-type._tcp.local."
+        name = "Xxxyyy"
+        registration_name = "%s.%s" % (name, type_)
+
+        desc = {'path': '/~paulsm/'}
+        info = ServiceInfo(
+            type_, registration_name, 80, 0, 0, desc, "ASH-2.local.", addresses=[socket.inet_aton("10.0.1.2")]
+        )
+
+        registry = r.ServiceRegistry()
+        registry.add(info)
+
+        assert registry.get_service_infos() == [info]
+        assert registry.get_info_name(registration_name.lower()) == info
+        assert registry.get_infos_type(type_.lower()) == [info]
+        assert registry.get_infos_server("ash-2.local.") == [info]
+        assert registry.get_types() == [type_.lower()]
+
+    def test_lookups_lower_case_by_upper_case(self):
+        type_ = "_test-srvc-type._tcp.local."
+        name = "xxxyyy"
+        registration_name = "%s.%s" % (name, type_)
+
+        desc = {'path': '/~paulsm/'}
+        info = ServiceInfo(
+            type_, registration_name, 80, 0, 0, desc, "ash-2.local.", addresses=[socket.inet_aton("10.0.1.2")]
+        )
+
+        registry = r.ServiceRegistry()
+        registry.add(info)
+
+        assert registry.get_service_infos() == [info]
+        assert registry.get_info_name(registration_name.upper()) == info
+        assert registry.get_infos_type(type_.upper()) == [info]
+        assert registry.get_infos_server("ASH-2.local.") == [info]
+        assert registry.get_types() == [type_]
