32b059525065d29912c92b7ef71f1165b1eaff9f
J. Nick Koston <nick@koston.org>
2021-06-15 10:39:06 -1000

    Add test for launching with apple_p2p=True

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,6 +9,7 @@ import logging
 import os
 import pytest
 import socket
+import sys
 import time
 import unittest
 import unittest.mock
@@ -98,6 +99,16 @@ class Framework(unittest.TestCase):
         rv.close()
         rv = r.Zeroconf(interfaces=r.InterfaceChoice.Default, ip_version=r.IPVersion.V6Only)
         rv.close()
+
+    @unittest.skipIf(sys.platform != 'darwin', reason="apple_p2p only available on mac")
+    def test_launch_and_close_apple_p2p(self):
+        rv = r.Zeroconf(apple_p2p=True)
+        rv.close()
+
+    @unittest.skipIf(sys.platform == 'darwin', reason="apple_p2p available on mac")
+    def test_launch_and_close_apple_p2p(self):
+        with pytest.raises(RuntimeError):
+            r.Zeroconf(apple_p2p=True)
 
     def test_handle_response(self):
         def mock_incoming_msg(service_state_change: r.ServiceStateChange) -> r.DNSIncoming:
