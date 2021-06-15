0ab8f2843f0b37ec1995fabc3ed4f50d7eef23c9
J. Nick Koston <nick@koston.org>
2021-06-15 13:50:30 -1000

    coverage

--- tests/utils/test_name.py
+++ tests/utils/test_name.py
@@ -10,8 +10,27 @@ from zeroconf._utils import name as nameutils
 from zeroconf import BadTypeInNameException
 
 
-def test_service_type_name():
-    """Test overlong service_type_name."""
+def test_service_type_name_overlong_type():
+    """Test overlong service_type_name type."""
     with pytest.raises(BadTypeInNameException):
         nameutils.service_type_name("Tivo1._tivo-videostream._tcp.local.")
     nameutils.service_type_name("Tivo1._tivo-videostream._tcp.local.", strict=False)
+
+
+def test_service_type_name_overlong_full_name():
+    """Test overlong service_type_name full name."""
+    with pytest.raises(BadTypeInNameException):
+        nameutils.service_type_name(
+            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
+            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
+            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
+            "._tivo-videostream._tcp.local."
+        )
+    with pytest.raises(BadTypeInNameException):
+        nameutils.service_type_name(
+            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
+            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
+            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
+            "._tivo-videostream._tcp.local.",
+            strict=False,
+        )
