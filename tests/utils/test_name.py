f6491b4db63f4b89c4bec9b70f7b18163008f2db
J. Nick Koston <nick@koston.org>
2021-06-15 13:58:31 -1000

    tweak test to cover

--- tests/utils/test_name.py
+++ tests/utils/test_name.py
@@ -19,18 +19,8 @@ def test_service_type_name_overlong_type():
 
 def test_service_type_name_overlong_full_name():
     """Test overlong service_type_name full name."""
+    long_name = "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1" * 100
     with pytest.raises(BadTypeInNameException):
-        nameutils.service_type_name(
-            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
-            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
-            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
-            "._tivo-videostream._tcp.local."
-        )
+        nameutils.service_type_name(f"{long_name}._tivo-videostream._tcp.local.")
     with pytest.raises(BadTypeInNameException):
-        nameutils.service_type_name(
-            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
-            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
-            "Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1Tivo1"
-            "._tivo-videostream._tcp.local.",
-            strict=False,
-        )
+        nameutils.service_type_name(f"{long_name}._tivo-videostream._tcp.local.", strict=False)
