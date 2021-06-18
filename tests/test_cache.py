cbe2ce398c440250001e7c2dfe0ab2e48e9cd40c
J. Nick Koston <nick@koston.org>
2021-06-18 10:29:24 -1000

    coverage

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -79,3 +79,59 @@ class TestDNSCacheAPI(unittest.TestCase):
         cache.async_add(record1)
         cache.async_add(record2)
         assert cache.get_by_details('a', const._TYPE_A, const._CLASS_IN) == record2
+
+    def test_get_all_by_details(self):
+        record1 = r.DNSAddress('a', const._TYPE_A, const._CLASS_IN, 1, b'a')
+        record2 = r.DNSAddress('a', const._TYPE_A, const._CLASS_IN, 1, b'b')
+        cache = r.DNSCache()
+        cache.async_add(record1)
+        cache.async_add(record2)
+        assert set(cache.get_all_by_details('a', const._TYPE_A, const._CLASS_IN)) == set([record1, record2])
+
+    def test_entries_with_server(self):
+        record1 = r.DNSService(
+            'irrelevant', const._TYPE_SRV, const._CLASS_IN, const._DNS_HOST_TTL, 0, 0, 85, 'ab'
+        )
+        record2 = r.DNSService(
+            'irrelevant', const._TYPE_SRV, const._CLASS_IN, const._DNS_HOST_TTL, 0, 0, 80, 'ab'
+        )
+        cache = r.DNSCache()
+        cache.async_add(record1)
+        cache.async_add(record2)
+        assert set(cache.entries_with_server('ab')) == set([record1, record2])
+
+    def test_entries_with_name(self):
+        record1 = r.DNSService(
+            'irrelevant', const._TYPE_SRV, const._CLASS_IN, const._DNS_HOST_TTL, 0, 0, 85, 'ab'
+        )
+        record2 = r.DNSService(
+            'irrelevant', const._TYPE_SRV, const._CLASS_IN, const._DNS_HOST_TTL, 0, 0, 80, 'ab'
+        )
+        cache = r.DNSCache()
+        cache.async_add(record1)
+        cache.async_add(record2)
+        assert set(cache.entries_with_name('irrelevant')) == set([record1, record2])
+
+    def test_current_entry_with_name_and_alias(self):
+        record1 = r.DNSPointer(
+            'irrelevant', const._TYPE_PTR, const._CLASS_IN, const._DNS_OTHER_TTL, 'x.irrelevant'
+        )
+        record2 = r.DNSPointer(
+            'irrelevant', const._TYPE_PTR, const._CLASS_IN, const._DNS_OTHER_TTL, 'y.irrelevant'
+        )
+        cache = r.DNSCache()
+        cache.async_add(record1)
+        cache.async_add(record2)
+        assert cache.current_entry_with_name_and_alias('irrelevant', 'x.irrelevant') == record1
+
+    def test_entries_with_name(self):
+        record1 = r.DNSService(
+            'irrelevant', const._TYPE_SRV, const._CLASS_IN, const._DNS_HOST_TTL, 0, 0, 85, 'ab'
+        )
+        record2 = r.DNSService(
+            'irrelevant', const._TYPE_SRV, const._CLASS_IN, const._DNS_HOST_TTL, 0, 0, 80, 'ab'
+        )
+        cache = r.DNSCache()
+        cache.async_add(record1)
+        cache.async_add(record2)
+        assert cache.names() == ['irrelevant']
