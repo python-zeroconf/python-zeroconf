3b9baf07278290b2b4eb8ac5850bccfbd8b107d8
J. Nick Koston <nick@koston.org>
2021-06-19 09:11:47 -1000

    Fix warning about Zeroconf._async_notify_all not being awaited in sync shutdown (#750)

--- zeroconf/_utils/aio.py
+++ zeroconf/_utils/aio.py
@@ -56,6 +56,7 @@ async def wait_condition_or_timeout(condition: asyncio.Condition, timeout: float
 
 async def _get_all_tasks(loop: asyncio.AbstractEventLoop) -> Set[asyncio.Task]:
     """Return all tasks running."""
+    await asyncio.sleep(0)  # flush out any call_soon_threadsafe
     if hasattr(asyncio, 'all_tasks'):
         return cast(Set[asyncio.Task], asyncio.all_tasks(loop))  # type: ignore  # pylint: disable=no-member
     return cast(Set[asyncio.Task], asyncio.Task.all_tasks(loop))  # type: ignore  # pylint: disable=no-member
