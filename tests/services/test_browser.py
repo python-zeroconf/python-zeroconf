15e36c930d5f6a827210fec7903f68cd1b667512
J. Nick Koston <nick@koston.org>
2021-06-19 18:35:16 -1000

    adjust test

--- tests/services/test_browser.py
+++ tests/services/test_browser.py
@@ -574,7 +574,7 @@ def test_integration():
                 time_offset += expected_ttl / 4
                 zeroconf_browser.notify_all()
                 sleep_count += 1
-                got_query.wait(0.1)
+                got_query.wait(0.5)
                 # Prevent the test running indefinitely in an error condition
                 assert sleep_count < test_iterations * 4
             assert not unexpected_ttl.is_set()
