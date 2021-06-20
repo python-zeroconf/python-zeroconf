6627be49af2ea3f344ac43a939ce02feb42d9ecd
J. Nick Koston <nick@koston.org>
2021-06-19 14:02:49 -1000

    Simplify ServiceBrowser callsbacks
    - We can fire the event or add to the queue that the
      thread is running right away in async_update_records_complete
      since there is no longer a concern about thread safety
      when the task is running in the event loop

--- zeroconf/_services/browser.py
+++ zeroconf/_services/browser.py
@@ -38,7 +38,7 @@ from .._services import (
     Signal,
     SignalRegistrationInterface,
 )
-from .._utils.aio import get_best_available_queue, get_running_loop, wait_condition_or_timeout
+from .._utils.aio import get_best_available_queue, get_running_loop
 from .._utils.name import service_type_name
 from .._utils.time import current_time_millis, millis_to_seconds
 from ..const import (
@@ -193,7 +193,6 @@ class _ServiceBrowserBase(RecordUpdateListener):
         self._next_time = {check_type_: current_time for check_type_ in self.types}
         self._delay = {check_type_: delay for check_type_ in self.types}
         self._pending_handlers: OrderedDict[Tuple[str, str], ServiceStateChange] = OrderedDict()
-        self._handlers_to_call: OrderedDict[Tuple[str, str], ServiceStateChange] = OrderedDict()
         self._service_state_changed = Signal()
         self.queue: Optional[queue.Queue] = None
         self.done = False
@@ -296,8 +295,19 @@ class _ServiceBrowserBase(RecordUpdateListener):
 
         This method will be run in the event loop.
         """
-        self._handlers_to_call.update(self._pending_handlers)
-        self._pending_handlers.clear()
+        while self._pending_handlers:
+            (name_type, state_change) = self._pending_handlers.popitem(False)
+
+            if self.queue:
+                self.queue.put((name_type, state_change))
+                continue
+
+            self._service_state_changed.fire(
+                zeroconf=self.zc,
+                service_type=name_type[1],
+                name=name_type[0],
+                state_change=state_change,
+            )
 
     def cancel(self) -> None:
         """Cancel the browser."""
@@ -307,8 +317,7 @@ class _ServiceBrowserBase(RecordUpdateListener):
     def generate_ready_queries(self) -> List[DNSOutgoing]:
         """Generate the service browser query for any type that is due."""
         now = current_time_millis()
-
-        if min(self._next_time.values()) > now:
+        if self._millis_to_wait(current_time_millis()):
             return []
 
         ready_types = []
@@ -323,55 +332,24 @@ class _ServiceBrowserBase(RecordUpdateListener):
 
         return generate_service_query(self.zc, now, ready_types, self.multicast)
 
-    def _seconds_to_wait(self) -> Optional[float]:
-        """Returns the number of seconds to wait for the next event."""
-        # If there are handlers to call
-        # we want to process them right away
-        if self._handlers_to_call:
-            return None
-
+    def _millis_to_wait(self, now: float) -> Optional[float]:
+        """Returns the number of milliseconds to wait for the next event."""
         # Wait for the type has the smallest next time
         next_time = min(self._next_time.values())
-        now = current_time_millis()
-
-        if next_time <= now:
-            return None
-
-        return millis_to_seconds(next_time - now)
+        return None if next_time <= now else next_time - now
 
     async def async_browser_task(self) -> None:
         """Run the browser task."""
         await self.zc.async_wait_for_start()
         assert self.zc.async_condition is not None
         while True:
-            timeout = self._seconds_to_wait()
+            timeout = self._millis_to_wait(current_time_millis())
             if timeout:
-                async with self.zc.async_condition:
-                    # We must check again while holding the condition
-                    # in case the other thread has added to _handlers_to_call
-                    # between when we checked above when we were not
-                    # holding the condition
-                    if not self._handlers_to_call:
-                        await wait_condition_or_timeout(self.zc.async_condition, timeout)
+                await self.zc.async_wait(timeout)
 
             outs = self.generate_ready_queries()
             for out in outs:
                 self.zc.async_send(out, addr=self.addr, port=self.port)
-
-            if not self._handlers_to_call:
-                continue
-
-            (name_type, state_change) = self._handlers_to_call.popitem(False)
-            if self.queue:
-                self.queue.put((name_type, state_change))
-                continue
-
-            self._service_state_changed.fire(
-                zeroconf=self.zc,
-                service_type=name_type[1],
-                name=name_type[0],
-                state_change=state_change,
-            )
 
     async def _async_cancel_browser(self) -> None:
         """Cancel the browser."""
