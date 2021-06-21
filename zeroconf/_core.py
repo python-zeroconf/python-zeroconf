c2918a2c3b121c4a35be9f3280219aef3a66610a
J. Nick Koston <nick@koston.org>
2021-06-21 08:48:33 -1000

    keep consistant

--- zeroconf/_core.py
+++ zeroconf/_core.py
@@ -138,7 +138,7 @@ class AsyncEngine:
             now = current_time_millis()
             self.zc.question_history.async_expire(now)
             self.zc.record_manager.async_updates(
-                now, [RecordUpdate(record, None) for record in self.zc.cache.async_expire(now)]
+                now, [RecordUpdate(record, record) for record in self.zc.cache.async_expire(now)]
             )
             self.zc.record_manager.async_updates_complete()
             await asyncio.sleep(millis_to_seconds(_CACHE_CLEANUP_INTERVAL))
