ebc23ee5e9592dd7f0235cd57f9b3ad727ec8bff
J. Nick Koston <nick@koston.org>
2021-08-12 21:42:50 -0500

    Sort responses to increase chance of name compression (#954)
    - When building an outgoing response, sort the names together
      to increase the likelihood of name compression. In testing
      this reduced the number of packets for large responses
      (from 7 packets to 6)

--- zeroconf/_handlers.py
+++ zeroconf/_handlers.py
@@ -100,12 +100,16 @@ def construct_outgoing_unicast_answers(
 
 def _add_answers_additionals(out: DNSOutgoing, answers: _AnswerWithAdditionalsType) -> None:
     # Find additionals and suppress any additionals that are already in answers
-    additionals: Set[DNSRecord] = set().union(*answers.values())  # type: ignore
-    additionals -= answers.keys()
-    for answer in answers:
+    sending: Set[DNSRecord] = set(answers.keys())
+    # Answers are sorted to group names together to increase the chance
+    # that similar names will end up in the same packet and can reduce the
+    # overall size of the outgoing response via name compression
+    for answer, additionals in sorted(answers.items(), key=lambda kv: kv[0].name):
         out.add_answer_at_time(answer, 0)
-    for additional in additionals:
-        out.add_additional_answer(additional)
+        for additional in additionals:
+            if additional not in sending:
+                out.add_additional_answer(additional)
+                sending.add(additional)
 
 
 def sanitize_incoming_record(record: DNSRecord) -> None:
