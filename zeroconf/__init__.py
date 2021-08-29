e8d84017b750ab5f159abc7225f9922d84a8f9fd
J. Nick Koston <nick@koston.org>
2021-08-29 13:41:54 -0500

    Bump version: 0.36.0 → 0.36.1

--- zeroconf/__init__.py
+++ zeroconf/__init__.py
@@ -80,7 +80,7 @@ from ._utils.time import current_time_millis, millis_to_seconds  # noqa # import
 
 __author__ = 'Paul Scott-Murphy, William McBrine'
 __maintainer__ = 'Jakub Stasiak <jakub@stasiak.at>'
-__version__ = '0.36.0'
+__version__ = '0.36.1'
 __license__ = 'LGPL'
 
 
