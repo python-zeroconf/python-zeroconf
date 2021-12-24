ff766345461a82547abe462b5d690621c755d480
J. Nick Koston <nick@koston.org>
2021-12-23 16:38:15 -1000

    Avoid linear type searches in ServiceBrowsers (#1044)

--- tests/utils/test_name.py
+++ tests/utils/test_name.py
@@ -23,3 +23,25 @@ def test_service_type_name_overlong_full_name():
         nameutils.service_type_name(f"{long_name}._tivo-videostream._tcp.local.")
     with pytest.raises(BadTypeInNameException):
         nameutils.service_type_name(f"{long_name}._tivo-videostream._tcp.local.", strict=False)
+
+
+def test_possible_types():
+    """Test possible types from name."""
+    assert nameutils.possible_types('.') == set()
+    assert nameutils.possible_types('local.') == set()
+    assert nameutils.possible_types('_tcp.local.') == set()
+    assert nameutils.possible_types('_test-srvc-type._tcp.local.') == {'_test-srvc-type._tcp.local.'}
+    assert nameutils.possible_types('_any._tcp.local.') == {'_any._tcp.local.'}
+    assert nameutils.possible_types('.._x._tcp.local.') == {'_x._tcp.local.'}
+    assert nameutils.possible_types('x.y._http._tcp.local.') == {'_http._tcp.local.'}
+    assert nameutils.possible_types('1.2.3._mqtt._tcp.local.') == {'_mqtt._tcp.local.'}
+    assert nameutils.possible_types('x.sub._http._tcp.local.') == {'_http._tcp.local.'}
+    assert nameutils.possible_types('6d86f882b90facee9170ad3439d72a4d6ee9f511._zget._http._tcp.local.') == {
+        '_http._tcp.local.',
+        '_zget._http._tcp.local.',
+    }
+    assert nameutils.possible_types('my._printer._sub._http._tcp.local.') == {
+        '_http._tcp.local.',
+        '_sub._http._tcp.local.',
+        '_printer._sub._http._tcp.local.',
+    }
