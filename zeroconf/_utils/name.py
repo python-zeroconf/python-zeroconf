c116eaca812fd7d4f58393a9b2f0f0f2c30c8dc5
J. Nick Koston <nick@koston.org>
2022-05-05 11:24:37 -0400

    tweak labels

--- zeroconf/_utils/name.py
+++ zeroconf/_utils/name.py
@@ -161,12 +161,13 @@ def service_type_name(type_: str, *, strict: bool = True) -> str:  # pylint: dis
 
 def possible_types(name: str) -> Set[str]:
     """Build a set of all possible types from a fully qualified name."""
-    labels = name.split('.')
-    label_count = len(labels)
-    types = set()
-    for count in range(label_count):
-        parts = labels[label_count - count - 4 :]
-        if not parts[0].startswith('_'):
+    type_labels = name.partition('_')[2]
+    if "_" not in type_labels:
+        return set()
+    types = {f"_{type_labels}"}
+    labels = type_labels.split('.')
+    for count in range(1, len(labels) - 3):
+        if not labels[count].startswith('_'):
             break
-        types.add('.'.join(parts))
+        types.add('.'.join(labels[count:]))
     return types
