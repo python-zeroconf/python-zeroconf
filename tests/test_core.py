d2d1489bc764b40fe5d1d9fde486aaca5c67e358
J. Nick Koston <nick@koston.org>
2022-05-06 12:41:54 -0500

    skip on 3.7

--- tests/test_core.py
+++ tests/test_core.py
@@ -801,6 +801,7 @@ def test_shutdown_while_register_in_process():
 
 
 @pytest.mark.asyncio
+@unittest.skipIf(sys.version_info[:3][1] < 8, 'Requires Python 3.8 or later')
 @patch("zeroconf._core._STARTUP_TIMEOUT", 0)
 @patch("zeroconf._core.AsyncEngine._async_setup")
 async def test_event_loop_blocked(mock_start):
