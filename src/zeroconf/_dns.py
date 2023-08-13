0b7602822de5a678219d68f36f9056d9844707cb
J. Nick Koston <nick@koston.org>
2023-08-13 15:49:13 -0500

    fix: tweak

--- src/zeroconf/_dns.py
+++ src/zeroconf/_dns.py
@@ -193,10 +193,8 @@ class DNSRecord(DNSEntry):
     # TODO: Switch to just int here
     def get_remaining_ttl(self, now: _float) -> Union[int, float]:
         """Returns the remaining TTL in seconds."""
-        remaining = self.created + (_EXPIRE_FULL_TIME_MS * self.ttl) - now
-        if remaining < 0:
-            return 0
-        return remaining / 1000.0
+        remain = (self.created + (_EXPIRE_FULL_TIME_MS * self.ttl) - now) / 1000.0
+        return 0 if remain < 0 else remain
 
     def is_expired(self, now: _float) -> bool:
         """Returns true if this record has expired."""
