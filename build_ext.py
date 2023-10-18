5ea328ec455cf9cb1cb4d662b59f6de60c84f4ae
J. Nick Koston <nick@koston.org>
2023-10-17 20:45:15 -1000

    fix: reduce size of wheels by excluding generated .c files

--- build_ext.py
+++ build_ext.py
@@ -44,6 +44,7 @@ def build(setup_kwargs: Any) -> None:
                 cmdclass=dict(build_ext=BuildExt),
             )
         )
+        setup_kwargs["exclude_package_data"] = {pkg: ["*.c"] for pkg in setup_kwargs["packages"]}
     except Exception:
         if os.environ.get("REQUIRE_CYTHON"):
             raise
