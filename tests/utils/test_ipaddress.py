62008c74dea628ee7a1c9fda9eeeb4b7b7fc0d68
J. Nick Koston <nick@koston.org>
2023-12-13 11:02:26 -1000

    chore: add missing cover

--- tests/utils/test_ipaddress.py
+++ tests/utils/test_ipaddress.py
@@ -13,3 +13,12 @@ def test_cached_ip_addresses_wrapper():
         str(ipaddress.cached_ip_addresses(b'&\x06(\x00\x02 \x00\x01\x02H\x18\x93%\xc8\x19F'))
         == '2606:2800:220:1:248:1893:25c8:1946'
     )
+    assert ipaddress.cached_ip_addresses('::1') == ipaddress.IPv6Address('::1')
+
+    ipv4 = ipaddress.cached_ip_addresses('169.254.0.0')
+    assert ipv4 is not None
+    assert ipv4.is_link_local is True
+
+    ipv6 = ipaddress.cached_ip_addresses('fe80::1')
+    assert ipv6 is not None
+    assert ipv6.is_link_local is True
