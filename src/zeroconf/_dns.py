2b0dc785f76c356bcad8dccb2679d1a44517cf83
J. Nick Koston <nick@koston.org>
2023-12-15 09:01:54 -1000

    fix: reduce precision loss

--- src/zeroconf/_dns.py
+++ src/zeroconf/_dns.py
@@ -195,8 +195,8 @@ class DNSRecord(DNSEntry):
 
     def get_percentage_remaining_ttl(self, now: _float) -> float:
         """Returns the percentage remaining of the ttl between 0-100."""
-        remain = (self.created + (_EXPIRE_FULL_TIME_MS * self.ttl) - now) / 1000.0
-        return 0 if remain < 0 else (remain / self.ttl) * 100
+        remain = (self.created + (_EXPIRE_FULL_TIME_MS * self.ttl) - now) / self.ttl / 10
+        return 0 if remain < 0 else remain
 
     # TODO: Switch to just int here
     def get_remaining_ttl(self, now: _float) -> Union[int, float]:
