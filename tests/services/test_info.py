d00375878cb0fe7dfd6e3487136586f57fbfd62c
Rotzbua <Rotzbua@users.noreply.github.com>
2025-01-08 12:40:57 +0100

    chore: remove legacy code <py39

--- tests/services/test_info.py
+++ tests/services/test_info.py
@@ -7,7 +7,6 @@ import asyncio
 import logging
 import os
 import socket
-import sys
 import threading
 import unittest
 from ipaddress import ip_address
@@ -704,7 +703,6 @@ def test_multiple_addresses():
     assert info.addresses == [address, address]
     assert info.parsed_addresses() == [address_parsed, address_parsed]
     assert info.parsed_scoped_addresses() == [address_parsed, address_parsed]
-    ipaddress_supports_scope_id = sys.version_info >= (3, 9, 0)
 
     if has_working_ipv6() and not os.environ.get("SKIP_IPV6"):
         address_v6_parsed = "2001:db8::1"
@@ -751,9 +749,7 @@ def test_multiple_addresses():
             assert info.ip_addresses_by_version(r.IPVersion.All) == [
                 ip_address(address),
                 ip_address(address_v6),
-                ip_address(address_v6_ll_scoped_parsed)
-                if ipaddress_supports_scope_id
-                else ip_address(address_v6_ll),
+                ip_address(address_v6_ll_scoped_parsed),
             ]
             assert info.addresses_by_version(r.IPVersion.V4Only) == [address]
             assert info.ip_addresses_by_version(r.IPVersion.V4Only) == [ip_address(address)]
@@ -763,9 +759,7 @@ def test_multiple_addresses():
             ]
             assert info.ip_addresses_by_version(r.IPVersion.V6Only) == [
                 ip_address(address_v6),
-                ip_address(address_v6_ll_scoped_parsed)
-                if ipaddress_supports_scope_id
-                else ip_address(address_v6_ll),
+                ip_address(address_v6_ll_scoped_parsed),
             ]
             assert info.parsed_addresses() == [
                 address_parsed,
@@ -780,16 +774,15 @@ def test_multiple_addresses():
             assert info.parsed_scoped_addresses() == [
                 address_parsed,
                 address_v6_parsed,
-                address_v6_ll_scoped_parsed if ipaddress_supports_scope_id else address_v6_ll_parsed,
+                address_v6_ll_scoped_parsed,
             ]
             assert info.parsed_scoped_addresses(r.IPVersion.V4Only) == [address_parsed]
             assert info.parsed_scoped_addresses(r.IPVersion.V6Only) == [
                 address_v6_parsed,
-                address_v6_ll_scoped_parsed if ipaddress_supports_scope_id else address_v6_ll_parsed,
+                address_v6_ll_scoped_parsed,
             ]
 
 
-@unittest.skipIf(sys.version_info < (3, 9, 0), "Requires newer python")
 def test_scoped_addresses_from_cache():
     type_ = "_http._tcp.local."
     registration_name = f"scoped.{type_}"
--- tests/utils/test_ipaddress.py
+++ tests/utils/test_ipaddress.py
@@ -2,10 +2,6 @@
 
 """Unit tests for zeroconf._utils.ipaddress."""
 
-import sys
-
-import pytest
-
 from zeroconf import const
 from zeroconf._dns import DNSAddress
 from zeroconf._utils import ipaddress
@@ -52,7 +48,6 @@ def test_cached_ip_addresses_wrapper():
     assert ipv6.is_unspecified is True
 
 
-@pytest.mark.skipif(sys.version_info < (3, 9, 0), reason="scope_id is not supported")
 def test_get_ip_address_object_from_record():
     """Test the get_ip_address_object_from_record."""
     # not link local
