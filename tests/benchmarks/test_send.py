b51bb4d6c6bb510c98764ebe2e471b857518e33b
J. Nick Koston <nick@koston.org>
2025-01-10 11:23:59 -1000

    chore: add benchmark for sending packets

---
+++ tests/benchmarks/test_send.py
@@ -0,0 +1,18 @@
+"""Benchmark for sending packets."""
+
+from pytest_codspeed import BenchmarkFixture
+
+from zeroconf.asyncio import AsyncZeroconf
+
+from .helpers import generate_packets
+
+
+async def test_sending_packets(benchmark: BenchmarkFixture) -> None:
+    """Benchmark sending packets."""
+    aiozc = AsyncZeroconf(interfaces=["127.0.0.1"])
+    await aiozc.zeroconf.async_wait_for_start()
+    out = generate_packets()
+
+    @benchmark
+    def _send_packets() -> None:
+        aiozc.zeroconf.async_send(out)
