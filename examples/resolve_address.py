edd86c879ad1eeef1f2a49432221298a7f08a006
J. Nick Koston <nick@koston.org>
2025-01-30 11:21:58 -0600

    chore: improve example

---
+++ examples/resolve_address.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""Example of resolving a name to an IPv4 address."""
+
+import asyncio
+import logging
+import sys
+
+from zeroconf import AddressResolver, IPVersion
+from zeroconf.asyncio import AsyncZeroconf
+
+
+async def resolve_name(name: str) -> None:
+    aiozc = AsyncZeroconf(interfaces=["127.0.0.1"])
+    await aiozc.zeroconf.async_wait_for_start()
+    resolver = AddressResolver(name)
+    if await resolver.async_request(aiozc.zeroconf, 3000):
+        print(f"{name} IP addresses:", resolver.ip_addresses_by_version(IPVersion.All))
+    else:
+        print(f"Name {name} not resolved")
+    await aiozc.async_close()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    argv = sys.argv.copy()
+    if "--debug" in argv:
+        logging.getLogger("zeroconf").setLevel(logging.DEBUG)
+        argv.remove("--debug")
+
+    if len(argv) < 2 or not argv[1]:
+        raise ValueError("Usage: resolve_address.py [--debug] <name>")
+
+    name = argv[1]
+    if not name.endswith("."):
+        name += "."
+
+    asyncio.run(resolve_name(name))
