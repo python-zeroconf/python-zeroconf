1d6de05d3c6a99c7d9a30171bb269b9a38c020c6
J. Nick Koston <nick@home-assistant.io>
2026-08-29 15:54:38 -0500

    revert: remove a derivative notify docstring

--- src/zeroconf/_core.py
+++ src/zeroconf/_core.py
@@ -269,7 +269,6 @@ class Zeroconf(QuietLogger):
         await wait_for_future_set_or_timeout(loop, self._notify_futures, timeout)
 
     def notify_all(self) -> None:
-        """Notifies all waiting threads and notify listeners."""
         assert self.loop is not None
         self.loop.call_soon_threadsafe(self.async_notify_all)
 
