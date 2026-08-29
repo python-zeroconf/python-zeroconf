fc101b410e8ad78e8f2ca9fd88cd52680e540fa7
J. Nick Koston <nick@koston.org>
2026-08-29 15:52:15 -0500

    chore: add a short license header pointing at COPYING (#1861)

--- src/zeroconf/_protocol/incoming.py
+++ src/zeroconf/_protocol/incoming.py
@@ -1,3 +1,8 @@
+"""A pure python implementation of multicast DNS service discovery.
+
+Licensed under LGPL-2.1-or-later; see COPYING for details.
+"""
+
 from __future__ import annotations
 
 import struct
--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,1 +1,6 @@
+"""A pure python implementation of multicast DNS service discovery.
+
+Licensed under LGPL-2.1-or-later; see COPYING for details.
+"""
+
 from __future__ import annotations
