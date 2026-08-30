0e7c22a9572b1310a2544f196137f434f6fa2887
J. Nick Koston <nick@koston.org>
2026-08-29 19:06:45 -0500

    chore: add modification notices to headers and require affirmative consent for past contributions (#1885)

--- src/zeroconf/_handlers/record_manager.py
+++ src/zeroconf/_handlers/record_manager.py
@@ -1,6 +1,8 @@
 """A pure python implementation of multicast DNS service discovery.
 
-Licensed under LGPL-2.1-or-later; see COPYING for details.
+Licensed under LGPL-2.1-or-later; see COPYING for details. This file is
+part of a continuously modified work; modification dates are recorded
+in the project's git history.
 """
 
 from __future__ import annotations
